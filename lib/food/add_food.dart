72eb13b5612e72be85137ec28767defe4a7341c6
yeeet7 <viktor.kovalik171@gmail.com>
2023-11-10 21:09:42 +0100

    search foods

--- lib/food/add_food.dart
+++ lib/food/add_food.dart
@@ -23,73 +23,77 @@ class _AddFoodState extends State<AddFood> {
 
     return Hero(
       tag: 'FAB',
-      child: Scaffold(
-    
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Theme.of(context).colorScheme.secondary,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios_new_rounded),
-            onPressed: () => Navigator.pop(context),
-          ),
-          title: TextField(
-            controller: addFoodController,
-            cursorColor: Colors.white54,
-            focusNode: addFoodNode,
-            onTapOutside: (event) => addFoodNode.unfocus(),
-            decoration: InputDecoration(
-              isDense: true,
-              filled: true,
-              hintText: 'Search',
-              contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
-              fillColor: Theme.of(context).colorScheme.primary,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide(color: Colors.white, width: .2)
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: const BorderSide(color: Colors.white, width: 1)
-              ),
-            ),
-          ),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => FoodWidget(FoodIntent.create, () => setState(() {})))),
-            )
-          ],
-        ),
-    
-        body: FutureBuilder(
-          future: FirebaseFirestore.instance.collection('config').doc(FirebaseAuth.instance.currentUser?.uid).collection('foods').get(),
-          builder: (context, snapshot) {
-            return SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: snapshot.data?.docs.map<Widget>(
-                  (e) => FoodTile(
-                    Food(
-                      id: e.id,
-                      name: e.data()['name'] ?? 'name',
-                      amount: e.data()['amount'] ?? 1,
-                      imagePath: e.data()['imagePath'],
-                      unit: Unit.values.elementAt(e.data()['unit']),
-                      kcal: e.data()['calories'] ?? 0,
-                      carbs: e.data()['carbs'] ?? 0.0,
-                      proteins: e.data()['proteins'] ?? 0.0,
-                      fats: e.data()['fats'] ?? 0.0
+      child: FutureBuilder(
+        future: FirebaseFirestore.instance.collection('config').doc(FirebaseAuth.instance.currentUser?.uid).collection('foods').get(),
+        builder: (context, snapshot) {
+          return StatefulBuilder(
+            builder: (context, setstate) {
+              return Scaffold(
+                appBar: AppBar(
+                  centerTitle: true,
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                  leading: IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  title: TextField(
+                    controller: addFoodController,
+                    cursorColor: Colors.white54,
+                    focusNode: addFoodNode,
+                    onChanged: (String text) => setstate(() {}),
+                    onTapOutside: (event) => addFoodNode.unfocus(),
+                    decoration: InputDecoration(
+                      isDense: true,
+                      filled: true,
+                      hintText: 'Search',
+                      contentPadding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                      fillColor: Theme.of(context).colorScheme.primary,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(color: Colors.white, width: .2)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(color: Colors.white, width: 1)
+                      ),
                     ),
-                    setstate: () {widget.setstate.call(); setState(() {});},
-                  )
-                ).toList() ?? [],
-                ///TODO: #1
-              ),
-            );
-          }
-        )
-    
-      ),
+                  ),
+                  actions: [
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => FoodWidget(FoodIntent.create, () => setState(() {})))),
+                    )
+                  ],
+                ),
+        
+                body: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: snapshot.data?.docs.where((element) => (element.data()['name'] as String).contains(addFoodController.text)).map<Widget>(
+                      (e) => FoodTile(
+                        Food(
+                          id: e.id,
+                          name: e.data()['name'] ?? 'name',
+                          amount: e.data()['amount'] ?? 1,
+                          imagePath: e.data()['imagePath'],
+                          unit: Unit.values.elementAt(e.data()['unit']),
+                          kcal: e.data()['calories'] ?? 0,
+                          carbs: e.data()['carbs'] ?? 0.0,
+                          proteins: e.data()['proteins'] ?? 0.0,
+                          fats: e.data()['fats'] ?? 0.0
+                        ),
+                        setstate: () {widget.setstate.call(); setState(() {});},
+                      )
+                    ).toList() ?? [],
+                    ///TODO: #1
+                  ),
+                )
+        
+              );
+            }
+          );
+        }
+      )
     );
   }
 }
